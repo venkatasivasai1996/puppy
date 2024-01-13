0b1f95024c1525b602a0eeb681e9d52b9105d003
venkatasivasai1996 <nadipeneni.siva@gmail.com>
2024-01-13 17:27:30 -0600

    push latest

--- puppy.go
+++ puppy.go
@@ -1,5 +1,9 @@
 package puppy
 
+import (
+	"github.com/venkatasivasai1996/dog"
+)
+
 func Bark() string {
 	return "first go"
 }
@@ -7,4 +11,10 @@ func Bark() string {
 func Barks() string {
 	return "woof! woof! woof!"
 }
+func Bigbark() string {
+	return dog.WhenGrownUP(Bark())
+}
 
+func Bigbarks() string {
+	return dog.WhenGrownUP(Barks())
+}
